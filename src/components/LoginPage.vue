520f12ef0d62358bd140b1507adf8506c5c70cf2
valentVasti <valentvasti2114@gmail.com>
2022-12-14 10:02:55 +0700

    buat project

---
+++ src/components/LoginPage.vue
@@ -0,0 +1,9 @@
+<template>
+    <p>Login Page</p>
+</template>
+  
+<script>
+  export default {
+
+  }
+</script>
---
+++ src/router.js
@@ -0,0 +1,50 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+
+function importComponent(path){
+    return () => import(`./components/${path}.vue`);
+}
+
+const router = new VueRouter({
+    mode: "history",
+    routes: [
+        {
+            path: "/",
+            name: "admin",
+            component: importComponent("LoginPage"),
+            // children: [
+            //     //Dashboard
+            //     {
+            //         path: "/",
+            //         name: "Root",
+            //         component: importComponent("DashboardIndex"),
+            //     },
+
+            //     //To Do List
+            //     {
+            //         path: "/gd",
+            //         name: "Guided",
+            //         component: importComponent("TodoList/ListItem"),
+            //     },
+
+            //     //UGD
+            //     {
+            //         path: "/ugd",
+            //         name: "Activity Unguided",
+            //         component: importComponent("TodoList/ToDoListUGD"),
+            //     },
+
+            //     //TGS
+            //     {
+            //         path: "/tgs",
+            //         name: "Activity Tugas",
+            //         component: importComponent("TodoList/TugasList"),
+            //     },
+            // ],
+        },
+    ],
+});
+
+export default router;
