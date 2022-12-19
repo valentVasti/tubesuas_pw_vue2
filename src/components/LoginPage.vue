9900bf40a2cf20913d98465fc0ccf06bda4eee78
xkendrickz <kendrickchandra128@gmail.com>
2022-12-20 01:02:19 +0700

    validasi login register

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-      <!-- <v-navigation-drawer app v-model="drawer" width="16%" color="#ff8600" hide-overlay>
+	<div>
+		<!-- <v-navigation-drawer app v-model="drawer" width="16%" color="#ff8600" hide-overlay>
           <v-list-item>
               <v-list-item-content>
                   <v-img contain max-height="60"></v-img>
@@ -18,151 +18,141 @@
               </v-list-item>
           </v-list>
       </v-navigation-drawer> -->
-        <div class="fluid">         
-            <v-app-bar app fixed height="65%" color="#219ebc">
-                <!-- <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon> -->
-                <span class="navbartext">
-                    <!-- XXXX: NPM -->
-                    Nama Website
-                </span>
-                <v-spacer></v-spacer>
-                <span class="navbartext">
-                    <!-- XXXX: NPM -->
-                    Login Page
-                </span>
-            </v-app-bar>
+		<div class="fluid">
+			<v-app-bar app fixed height="65%" color="#219ebc">
+				<!-- <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon> -->
+				<span class="navbartext">
+					<!-- XXXX: NPM -->
+					Nama Website
+				</span>
+				<v-spacer></v-spacer>
+				<span class="navbartext">
+					<!-- XXXX: NPM -->
+					Login Page
+				</span>
+			</v-app-bar>
 
-            <v-card
-                class="cardLogin"
-                max-width="400"
-                elevation="5"
-                rounded
-            >
-                <v-card-title class="logincardtitle">LOGIN</v-card-title>
-                <v-card-subtitle>Let's get started!</v-card-subtitle>
+			<v-card class="cardLogin" max-width="400" elevation="5" rounded>
+				<v-card-title class="logincardtitle">LOGIN</v-card-title>
+				<v-card-subtitle>Let's get started!</v-card-subtitle>
 
-                <v-text-field
-                    v-model="name"
-                    label="Name"
-                    required
-                    filled
-                    rounded
-                    dense
-                ></v-text-field>
+				<v-form ref="form" @submit.prevent="submit">
+					<v-container fluid>
+						<v-row>
 
-                <v-text-field
-                    v-model="password"
-                    label="Password"
-                    required
-                    filled
-                    rounded
-                    dense
-                ></v-text-field>
+							<v-text-field v-model="name" label="Name" :rules="empty" required filled rounded
+								dense></v-text-field>
+							<v-text-field v-model="password" label="Password" :rules="empty" required filled rounded
+								dense></v-text-field>
+						</v-row>
+					</v-container>
+					<v-spacer></v-spacer>
+					<v-btn color="#219ebc" rounded large block type="submit" @click="store()">
+						Sign In
+					</v-btn>
 
-                <v-btn color="#219ebc" rounded large block>
-                    <span class="loginBtn">Sign In</span>
-                </v-btn>
-                <p></p>
-                <hr class="mx-auto" width="350px"/>
-                <p></p>
-                <v-btn color="secondary" text rounded large block to="/register">
-                    <span class="dontBtn">Don't Have Account?</span>
-                </v-btn>
-            </v-card>            
-        </div>
-    </div>
+					<p></p>
+					<hr class="mx-auto" width="350px" />
+					<p></p>
+					<v-btn color="secondary" text rounded large block to="/register">
+						<span class="dontBtn">Don't Have Account?</span>
+					</v-btn>
+				</v-form>
+			</v-card>
+		</div>
+	</div>
 </template>
 
 <style scoped>
-  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400&display=swap');
-  @import url('https://fonts.googleapis.com/css2?family=Urbanist:wght@500&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Urbanist:wght@500&display=swap');
 
-  .navbartext{
-      font-family: Poppins; 
-      font-style:bold; 
-      color: rgb(255, 255, 255);
-      font-size: 20px;
-  }
-  .textfield{
-      font-family: Poppins !important;
-      color: black;
-  }
+.navbartext {
+	font-family: Poppins;
+	font-style: bold;
+	color: rgb(255, 255, 255);
+	font-size: 20px;
+}
 
-  .cardLogin{
-    margin-top: 150px;
-    margin-left: auto;
-    margin-bottom: auto;
-    margin-right: auto;
-    padding: 1%;
+.textfield {
+	font-family: Poppins !important;
+	color: black;
+}
 
-  }
-  .logincardtitle{
-    font-weight: 700;
-    font-size: xx-large;
-    font-family: Poppins !important;
-  }
-  .loginBtn{
-    font-family: Poppins !important; 
-    font-style:bold; 
-    color: rgb(255, 255, 255);
-    font-size: large;
-  }
+.cardLogin {
+	margin-top: 150px;
+	margin-left: auto;
+	margin-bottom: auto;
+	margin-right: auto;
+	padding: 1%;
 
-  .dontBtn{
-    font-family: Poppins !important; 
-    font-style:bold; 
-    color: rgb(59, 59, 59);
-    font-size: large;    
-  }
+}
+
+.logincardtitle {
+	font-weight: 700;
+	font-size: xx-large;
+	font-family: Poppins !important;
+}
+
+.loginBtn {
+	font-family: Poppins !important;
+	font-style: bold;
+	color: rgb(255, 255, 255);
+	font-size: large;
+}
+
+.dontBtn {
+	font-family: Poppins !important;
+	font-style: bold;
+	color: rgb(59, 59, 59);
+	font-size: large;
+}
 </style>
 
 <script>
 // import { ref, reactive } from 'vue'
-// // import { useRoute } from 'vue-router'
+// import { useRoute } from 'vue-router'
 // import axios from 'axios'
 
-// export default {
-//     setup(){
-//         //state departemen
-//         const login = reactive({
-//             username: '',
-//             password: '',
-//         })
-    
-//         //state validation
-//         const validation = ref([])
+export default {
+	setup() {
+		//state departemen
+		// const login = reactive({
+		// 	username: '',
+		// 	password: '',
+		// })
 
-//         //method store
-//         function login() {
-//             let username = user.username
-//             let password = user.password
+		// //state validation
+		// const validation = ref([])
 
-//             axios.post('http://127.0.0.1:8000/api/users/login', {
-//                 username: username,
-//                 password: password,
-//             }).then(() => {
-//                 //redirect ke post index
-//                 this.$router.push({
-//                     name: 'loginPage'
-//                 })
-//             }).catch(error => {
-//                 //assign state validation with error
-//                 this.validation.value = error.response.data
-//                 console.log(error);
-//                 console.log("ERROR:: ",error.response.data)
-//             })
-//         }
+		// //method store
+		// function login() {
+		// 	let username = user.username
+		// 	let password = user.password
 
-//         //return
-//         return {
-//             user,
-//             validation,
-//             store
-//         }
-//     }
-// }
+		// 	axios.post('http://127.0.0.1:8000/api/users/login', {
+		// 		username: username,
+		// 		password: password,
+		// 	}).then(() => {
+		// 		//redirect ke post index
+		// 		this.$router.push({
+		// 			name: 'loginPage'
+		// 		})
+		// 	}).catch(error => {
+		// 		//assign state validation with error
+		// 		this.validation.value = error.response.data
+		// 		console.log(error);
+		// 		console.log("ERROR:: ", error.response.data)
+		// 	})
+		// }
 
-export default{
-    
+		//return
+		return {
+			empty: [val => (val || '').length > 0 || 'This field is required'],
+			// user,
+			// validation,
+			// store
+		}
+	},
 }
 </script>
