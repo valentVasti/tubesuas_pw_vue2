f4634a1c425da8e68ffb012a3282846474b078bc
thompluk <thomasdany38@gmail.com>
2022-12-21 23:05:12 +0700

    profil

--- src/components/User/ProfilePageUser.vue
+++ src/components/User/ProfilePageUser.vue
@@ -12,12 +12,12 @@
           ></v-img>
 
           <v-divider class="mx-4"></v-divider>
-          <v-card-text>Username:</v-card-text>
-          <v-card-text>Email:</v-card-text>
-          <v-card-text>Telepon:</v-card-text>
+          <v-text-field v-model="user.username" label="Username" disabled></v-text-field>
+          <v-text-field v-model="user.email" label="Email" disabled></v-text-field>
+          <v-text-field v-model="user.noTelp" label="Nomor Telepon" disabled></v-text-field>
 
           <v-card-actions>
-            <v-btn color="#219ebc" style="font-family: Poppins; font-size: 20px; font-weight: 700; color:#ffff;" @click="(dialogEdit = true), editItem(users)">Edit</v-btn>
+            <v-btn color="#219ebc" style="font-family: Poppins; font-size: 20px; font-weight: 700; color:#ffff;" @click="editData()">Edit</v-btn>
           </v-card-actions>            
 
       </v-card>
@@ -27,15 +27,13 @@
       <v-card color="brown lighten-5">
         <v-card-title>Edit Profile</v-card-title>
         <v-card-text>
-          <v-text-field label="Nama" v-model="formUser.username" required></v-text-field>
-          <v-text-field label="E-mail" v-model="formUser.email" required></v-text-field>
-          <v-text-field type="password" label="Password" v-model="formUser.password" required></v-text-field>
-          <v-text-field type="noTelp" label="noTelp" v-model="formUser.noTelp" required></v-text-field>
-  
+          <v-text-field v-model="users.username" label="Username" required :rules="inputRules"></v-text-field>
+          <v-text-field v-model="users.noTelp" label="Nomor Telepon" required :rules="inputRules"></v-text-field>
+          
           <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="brown lighten-1" class="mr-5" @click="cancel">Cancel</v-btn>
-          <v-btn color="green" @click="setProfile"> Save </v-btn>
+          <v-btn color="brown lighten-1" class="mr-5" @click="dialog = false">Cancel</v-btn>
+          <v-btn color="green" @click="save()"> Save </v-btn>
         </v-card-actions>
         </v-card-text>
       </v-card>
@@ -58,6 +56,84 @@
 </style>
 
 <script>
+
+import axios from 'axios'
+import { onMounted, ref, reactive } from 'vue'
+
+export default {
+    data(){
+        return{
+            dialog: false,
+            form: {
+              username: '',
+              noTelp:'',
+            },
+            inputRules: [
+                (v) => !!v || 'Must be Filled!'
+            ]
+        }
+    },
+    setup() {
+        //reactive state
+        let user = ref([])
+
+        onMounted(() => {
+            //get API from Laravel Backend
+            axios.get('http://127.0.0.1:8000/api/users/' + localStorage.getItem("id_user")).then(response => {
+                //assign state posts with response data
+                user.value = response.data.data
+            }).catch(error => {
+                console.log(error.response.data)
+            })
+        })        
+
+        axios.defaults.headers.common["Authorization"] =
+            localStorage.getItem("token_type") + " " + localStorage.getItem("token");
+
+        const users = reactive({
+            username: '',
+            noTelp:'',
+        })
+
+        const validation = ref([])
+
+        function save(){
+            
+                let username = users.username
+                let noTelp = users.noTelp
+
+                axios.put('http://127.0.0.1:8000/api/kotas/' + localStorage.getItem("id_user"), {
+                    username: username,
+                    noTelp: noTelp,
+                }).then(()=>{
+                    this.dialog = false
+                    window.location.reload()
+                }).catch(error=>{
+                    this.validation.value = error.response.data
+                    console.log(error);
+                    console.log("ERROR:: ", error.response.data)
+                })
+        }
+
+        function editData(){
+            this.dialog = true;
+            users.username = this.user.username
+            users.noTelp = this.user.noTelp
+        }
+        //return
+        return {
+            user,
+            users,
+            validation,
+            save,
+            editData
+        }
+    },
+  }
+
+</script>
+
+<!-- <script>
 
 import axios from 'axios'
 
@@ -180,4 +256,4 @@ export default {
     this.readData();
   },
 };
-</script> 
+</script>  -->
